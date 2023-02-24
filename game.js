7b1594ae96267a50048967e099b8b0988598417e
MhamadMhaisen <mheisensafe@gmail.com>
2023-02-24 18:42:21 +0200

    added bonus

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function () {
 
     const boundaries = document.getElementsByClassName("boundary");
@@ -8,60 +7,116 @@ document.addEventListener('DOMContentLoaded', function () {
     const game = document.getElementById("game");
     let score = 0;
     const scoreBox = document.createElement("div");
+    scoreBox.style.display = "flex";
+    scoreBox.style.justifyContent = "center";
+    scoreBox.style.alignItems = "center";
+    // Set the initial position of the player
+    const startPosition = {
+        x: player.offsetLeft,
+        y: player.offsetTop
+    };
+
     scoreBox.id = "scoreBox";
     document.body.appendChild(scoreBox);
-    let gameFinished = false;
 
+    let hasWon = false; // boolean flag to track whether player has won or not
+    let gameOver = false; // boolean flag to track whether the game is over or not
 
     player.addEventListener("mouseup", function () {
         addBoundaries();
         addWin();
         resetWalls();
         game.addEventListener("mouseleave", function () {
-
-            status.innerHTML = "CHEATER!";
-            for (let i = 0; i < boundaries.length; i++) {
-                boundaries[i].style.backgroundColor = "red";
-            }
-
-        })
-
-    })
-
-    // add mouseover event listener to each boundary element
-    function addBoundaries() {
-
-        for (let i = 0; i < boundaries.length; i++) {
-            boundaries[i].addEventListener("mouseover", function () {
-                status.innerHTML = "You LOST! retry by moving your mouse over the 'S'";
-
+            if (!hasWon && !gameOver) { // only show "CHEATER" message if player has not won and the game is not over
+                status.innerHTML = "CHEATER!";
                 for (let i = 0; i < boundaries.length; i++) {
                     boundaries[i].style.backgroundColor = "red";
                 }
+                score -= 10;
+                scoreBox.innerHTML = score;
+                console.log(score);
+                gameOver = true;
+                resetPlayer();
+            }
+        });
+    });
+
+    // add mouseover event listener to each boundary element
+    function addBoundaries() {
+        for (let i = 0; i < boundaries.length; i++) {
+            boundaries[i].addEventListener("mouseover", function () {
+                if (!hasWon && !gameOver) { // only change color of boundaries to red if player has not won and the game is not over
+                    status.innerHTML = "You LOST! retry by moving your mouse over the 'S'";
+                    for (let i = 0; i < boundaries.length; i++) {
+                        boundaries[i].style.backgroundColor = "red";
+                    }
+                    score -= 10;
+                    scoreBox.innerHTML = score;
+                    console.log(score);
+                    gameOver = true;
+                    resetPlayer();
+                }
             });
-
         }
-
-
     }
+
     //winning situation
     function addWin() {
         end.addEventListener("mouseover", function () {
-            if (boundaries[0].style.backgroundColor != "red") {
-                status.innerHTML = "You WON!"
+            if (!hasWon && !gameOver) { // only update score and status message if player has not won and the game is not over
+                hasWon = true;
+                status.innerHTML = "You WON!";
                 score += 5;
-                console.log(score)
+                scoreBox.innerHTML = score;
+                console.log(score);
+                gameOver = true;
+                resetPlayer();
             }
         });
     }
 
     function resetWalls() {
-        status.innerHTML = "Move your mouse towards the end"
+        status.innerHTML = "Move your mouse towards the end";
         for (let i = 0; i < boundaries.length; i++) {
             boundaries[i].style.backgroundColor = "#eee";
         }
+        hasWon = false;
+        gameOver = false; // reset the gameOver flag
     }
-    scoreBox.innerHTML = score;
+
+    function resetPlayer() {
+        player.style.left = startPosition.x + "px";
+        player.style.top = startPosition.y + "px";
+        gameStarted = false; // reset the gameStarted flag
+    }
+
+
+    let gameStarted = false;
+
+    player.addEventListener("mousedown", function () {
+        gameStarted = true;
+    });
+
+    game.addEventListener("mousemove", function (event) {
+        if (!gameStarted) {
+
+
+            return; // Don't update player position if game has not started
+        }
+
+        // Get the offset of the game element
+        const rect = game.getBoundingClientRect();
+
+        // Subtract the offset from the mouse position
+
+
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+        // Update the position of the player to match the position of the cursor
+        player.style.left = x + "px";
+        player.style.top = y + "px";
+    });
+
 
 }, false);
-
